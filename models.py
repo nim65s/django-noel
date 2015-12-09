c6e0ee965da3da5a31a86c3f890d1253b8372a2c
Guilhem Saurel <guilhem.saurel@gmail.com>
2015-12-10 00:49:07 +0100

    closed

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import Group, User
-from django.db.models import Model, CommaSeparatedIntegerField, ForeignKey
+from django.db.models import Model, CommaSeparatedIntegerField, ForeignKey, BooleanField
 
 
 class ChristmasGroup(Model):
     group = ForeignKey(Group, unique=True)
-    rotation = CommaSeparatedIntegerField(max_length=256, default='')  # 89 guys max…
+    rotation = CommaSeparatedIntegerField(max_length=256, default='', blank=True)  # 89 guys max…
+    closed = BooleanField(default=False)
 
     def save(self, *args, **kwargs):
         guys = self.group.user_set.order_by('?').values_list('pk', flat=True)
--- views.py
+++ views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 
 @login_required
 def secret_santa(request):
-    return render(request, 'secret_santa.html', context={'g': [(g.group, g.gift(request.user.pk)) for g in
-        [g.christmasgroup_set.first() for g in request.user.groups.all() if g.christmasgroup_set.exists()]]})
+    return render(request, 'secret_santa.html', context={'g':
+        [(g, g.gift(request.user.pk) if g.closed else g.group.user_set.all()) for g in
+            [g.christmasgroup_set.first() for g in request.user.groups.all() if g.christmasgroup_set.exists()]]})
